8233debeff98e43d276f00910fdf23301d8af420
YOUNG HO CHA <ganachoco@linecorp.com>
2022-10-04 00:48:40 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Directory structure
+
+* java : Project which contains java sources only
+* kotlin : Project which contains kotlin sources only
+* tools : Python script to generate source codes
+
+# Build Performance Result
+
+```
+$ ./gradlew clean
+
+> Task :clean
+> Task :java:clean
+> Task :kotlin:clean
+
+BUILD SUCCESSFUL in 481ms
+3 actionable tasks: 3 executed
+
+
+$ ./gradlew :java:assemble
+
+> Task :java:compileJava
+> Task :java:processResources NO-SOURCE
+> Task :java:classes
+> Task :java:jar
+> Task :java:assemble
+
+BUILD SUCCESSFUL in 3s
+2 actionable tasks: 2 executed
+
+
+$ ./gradlew :kotlin:assemble
+
+> Task :kotlin:processResources NO-SOURCE
+> Task :kotlin:compileKotlin
+> Task :kotlin:compileJava NO-SOURCE
+> Task :kotlin:classes UP-TO-DATE
+> Task :kotlin:jar
+> Task :kotlin:inspectClassesForKotlinIC
+> Task :kotlin:assemble
+
+BUILD SUCCESSFUL in 1m 22s
+3 actionable tasks: 3 executed
+
+```
---
+++ tools/gen_kt.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# vim: ts=4 sw=4 sts=4 et ai
+
+import os
+import shutil
+
+CLASS_HEADER = """
+package {package}
+
+class {klass_name}
+"""
+
+CLASS_FOOTER = """
+}
+"""
+
+
+def generate_source(basedir, package, klass_name):
+    output = os.path.join(*package.split('.'))
+    output = os.path.join(basedir, output, klass_name + '.kt')
+    with open(output, 'w') as fp:
+        fp.write(CLASS_HEADER.format(
+            package=package,
+            klass_name=klass_name
+        ) + "{\n")
+        for i in range(2000):
+            fp.write(f"var field{i}: Int = 1\n")
+
+        fp.write(CLASS_FOOTER)
+
+
+if __name__ == "__main__":
+    package = "com.example"
+    basedir = 'kotlin/src/main/java'
+
+    shutil.rmtree(basedir, True)
+    os.makedirs(os.path.join(basedir, os.path.join(*package.split('.'))))
+
+    for i in range(200):
+        klass_name = f"Klass{i}"
+        generate_source(basedir, package, klass_name)
